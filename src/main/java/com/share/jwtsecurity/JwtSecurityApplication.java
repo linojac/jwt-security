6a593beefb4b5178de2ef3f23dfb1f91cf468f86
Lino Jacob P J <linojac@gmail.com>
2019-04-03 08:15:31 +0530

    For ading this module as common dependency

--- src/main/java/com/share/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/com/share/jwtsecurity/JwtSecurityApplication.java
@@ -1,61 +1,13 @@
 package com.share.jwtsecurity;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.share.jwtsecurity.model.ApplicationUser;
-import com.share.jwtsecurity.model.UserRole;
-import com.share.jwtsecurity.repository.ApplicationUserRepository;
-import com.share.jwtsecurity.repository.UserRoleRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-@SpringBootApplication
+//@SpringBootApplication
 public class JwtSecurityApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(JwtSecurityApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(JwtSecurityApplication.class, args);
     }
 
-    @Bean
-    ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
-
-    @Bean
-    BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(MongoOperations mongoOperations, ApplicationUserRepository applicationUserRepository,
-                                        UserRoleRepository userRoleRepository) {
-        if (mongoOperations.collectionExists(ApplicationUser.class)) {
-            mongoOperations.dropCollection(ApplicationUser.class);
-        }
-        ApplicationUser user = new ApplicationUser();
-        user.setUsername("lino");
-        user.setPassword(bCryptPasswordEncoder().encode("lino"));
-
-        UserRole role = new UserRole();
-        role.setName("ADMIN");
-        role.setRolePrivileges(new ArrayList<>());
-        role.getRolePrivileges().add("READ");
-        role.getRolePrivileges().add("WRITE");
-        userRoleRepository.save(role);
-        userRoleRepository.findAll().forEach(r -> LOGGER.info(r.getName()));
-        user.setUserRoles(Arrays.asList("ADMIN"));
-        applicationUserRepository.save(user);
-        applicationUserRepository.findAll().forEach(u -> LOGGER.info(u.getUsername()));
-        return null;
-    }
 }
---
+++ src/main/java/com/share/jwtsecurity/config/AppConfig.java
@@ -0,0 +1,55 @@
+package com.share.jwtsecurity.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.share.jwtsecurity.model.ApplicationUser;
+import com.share.jwtsecurity.model.UserRole;
+import com.share.jwtsecurity.repository.ApplicationUserRepository;
+import com.share.jwtsecurity.repository.UserRoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Configuration
+public class AppConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(MongoOperations mongoOperations, ApplicationUserRepository applicationUserRepository,
+                                        UserRoleRepository userRoleRepository) {
+        if (mongoOperations.collectionExists(ApplicationUser.class)) {
+            mongoOperations.dropCollection(ApplicationUser.class);
+        }
+        ApplicationUser user = new ApplicationUser();
+        user.setUsername("lino");
+        user.setPassword(bCryptPasswordEncoder().encode("lino"));
+
+        UserRole role = new UserRole();
+        role.setName("ADMIN");
+        role.setRolePrivileges(new ArrayList<>());
+        role.getRolePrivileges().add("READ");
+        role.getRolePrivileges().add("WRITE");
+        userRoleRepository.save(role);
+        userRoleRepository.findAll().forEach(r -> LOGGER.info(r.getName()));
+        user.setUserRoles(Arrays.asList("ADMIN"));
+        applicationUserRepository.save(user);
+        applicationUserRepository.findAll().forEach(u -> LOGGER.info(u.getUsername()));
+        return null;
+    }
+}
